1ad10a7c65ea97b50d9722149bb444c5da3b01d1
Halil Zabun <halil.zabun@icloud.com>
2024-02-05 22:30:22 +0100

    feat: basic memory module

---
+++ memory.py
@@ -0,0 +1,16 @@
+from prompt import create_summary_prompt
+
+class summary_memory():
+
+    def save_context(self, new_summary: str):
+        pass
+
+    def load_context(self):
+        pass
+
+    def summarize_context(self, current_summary: str, new_lines: str, llm):
+        summary_prompt = create_summary_prompt(current_summary, new_lines)
+
+        #new_summary = llm.predict(summary_prompt)
+
+        #self.save_context(new_summary)
