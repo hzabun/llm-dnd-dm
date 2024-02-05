43ea7b2405c7ff10aedc8457e2be3b07b23b64c4
Halil Zabun <halil.zabun@icloud.com>
2024-02-05 22:20:26 +0100

    refactor: use llama-cpp-python without langchain

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-from langchain_openai import OpenAI
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationSummaryMemory
+from llama_cpp import Llama
+from memory import summary_memory
 
-llm = OpenAI(temperature=0)
+memory = summary_memory()
 
-conversation_with_summary = ConversationChain(
-    llm=llm,
-    memory=ConversationSummaryMemory(llm=OpenAI()),
-    verbose=True
-)
-
-conversation_with_summary.predict(input="Hi, what's up?")
-
-
-#memory.save_context({"input": "hi"}, {"output": "whats up"})
-#memory.load_memory_variables({})
