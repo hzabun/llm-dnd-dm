b31e2c47a716d6108077e949b555d2964f7457f3
Halil Zabun <halil.zabun@icloud.com>
2024-02-11 17:44:56 +0100

    fix: missing chat history files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from llama_cpp import Llama
 from memory import MessagesMemory
 
+
 llm = Llama(
     model_path="./llm_weights/openchat_3.5.Q4_K_M.gguf",
     n_ctx=1024,
@@ -20,6 +21,7 @@ prompt = memory.create_prompt_with_history(
 prompt_from_scratch = memory.create_new_chat_prompt(
     system_message="You are a worldclass chess player knowing all the tricks, but you're very busy at the moment drinking your coffee and don't want to be disturbed by anyone.",
     user_message="What is the purpose of the rook piece in chess?",
+    session="test",
 )
 
 
@@ -37,4 +39,4 @@ print(output["usage"])  # type: ignore
 
 chat_answer = output["choices"][0]["text"]  # type: ignore
 
-memory.save_messages_on_disk(chat_answer)
+memory.save_messages_on_disk(chat_answer, session="test", new_chat=False)
--- memory.py
+++ memory.py
@@ -1,38 +1,56 @@
 from prompt import create_summary_prompt
 import json
-from typing import Any, List, Dict, Tuple
+from typing import Any, List, Dict, Union
+import glob
 
 
 class MessagesMemory:
 
-    def save_messages_on_disk(self, message) -> None:
-        with open("history_logs/chess_message_lines.json", "r") as f:
-            data = json.load(f)
-            data.append(message)
+    chat_sessions: List[str] = []
 
-        with open("history_logs/chess_message_lines.json", "w") as f:
+    def save_messages_on_disk(
+        self,
+        messages: Union[List[Dict[str, str]], Dict[str, str]],
+        session: str,
+        new_chat: bool,
+    ) -> None:
+
+        if new_chat:
+            data = messages
+
+        else:
+            with open(
+                "history_logs/basic_message_lines/" + session + ".json", "r"
+            ) as f:
+                data = json.load(f)
+                data.append(messages)
+
+        with open("history_logs/basic_message_lines/" + session + ".json", "w") as f:
             json.dump(data, f, indent=4)
 
-    def load_messages_from_disk(self) -> List[Any]:
-        with open("history_logs/chess_message_lines.json") as f:
+    def load_messages_from_disk(self, session: str) -> List[Any]:
+
+        with open("history_logs/basic_message_lines/" + session + ".json", "r") as f:
             data = json.load(f)
 
         return data
 
-    def create_prompt_with_history(self, user_message: str) -> List[Any]:
+    def create_prompt_with_history(self, user_message: str, session: str) -> List[Any]:
+
         formatted_message = self.add_role_to_message(user_message, "user")
-        self.save_messages_on_disk(formatted_message)
-        prompt_with_history = self.load_messages_from_disk()
+        self.save_messages_on_disk(formatted_message, session, new_chat=False)
+        prompt_with_history = self.load_messages_from_disk(session)
 
         return prompt_with_history
 
     def create_new_chat_prompt(
-        self, system_message: str, user_message: str
+        self, system_message: str, user_message: str, session: str
     ) -> List[Any]:
+
         formatted_system_message = self.add_role_to_message("system", system_message)
         formatted_user_message = self.add_role_to_message("user", user_message)
         formatted_messages = [formatted_system_message, formatted_user_message]
-        self.save_messages_on_disk(formatted_messages)
+        self.save_messages_on_disk(formatted_messages, session, new_chat=True)
 
         return formatted_messages
 
