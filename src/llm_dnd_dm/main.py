5bff9ea97bf47acd4dffb450bdb5501a94c8cdb0
Halil Zabun <halil.zabun@icloud.com>
2024-02-21 15:17:21 +0100

    refactor: wrap inferences in functions

--- src/llm_dnd_dm/main.py
+++ src/llm_dnd_dm/main.py
@@ -1,43 +1,79 @@
 from llama_cpp import Llama
 from memory import MessagesMemory
-
-"""
-llm = Llama(
-    model_path="./llm_weights/openchat_3.5.Q4_K_M.gguf",
-    n_ctx=1024,
-    chat_format="openchat",
-    verbose=False,
-)
-"""
-
-memory = MessagesMemory()
+from typing import Any
 
 
-prompt = memory.create_prompt_with_history(
-    user_message="Alright, I guess I'll ask ChatGPT then if you're too busy with your coffee.",
-    session="test",
-)
+def setup_llm(
+    model_path: str, n_ctx: int, chat_format: str, verbose: bool = False
+) -> Any:
+
+    llm = Llama(
+        model_path=model_path,
+        n_ctx=n_ctx,
+        chat_format=chat_format,
+        verbose=verbose,
+    )
+    return llm
 
 
-prompt_from_scratch = memory.create_new_chat_prompt(
-    system_message="You are a worldclass chess player knowing all the tricks, but you're very busy at the moment drinking your coffee and don't want to be disturbed by anyone.",
-    user_message="What is the purpose of the rook piece in chess?",
-    session="test",
-)
+def start_chat():
+
+    memory = MessagesMemory()
+    prompt_from_scratch = memory.create_new_chat_prompt(
+        system_message="You are a worldclass chess player knowing all the tricks, but you're very busy at the moment drinking your coffee and don't want to be disturbed by anyone.",
+        user_message="What is the purpose of the rook piece in chess?",
+        session="test",
+    )
+
+    llm = setup_llm(
+        ".llm_weights/openchat_3.5.04_K_M-gguf",
+        n_ctx=102,
+        chat_format="openchat",
+        verbose=False,
+    )
+
+    output = llm.create_chat_completion(
+        messages=prompt_from_scratch,
+        max_tokens=None,
+        stop=[
+            "<|end_of_turn|>"
+        ],  # Stop generating just before the model would generate a new question
+    )
+
+    print(output["choices"])  # type: ignore
+    print(output["usage"])  # type: ignore
+
+    chat_answer = output["choices"][0]["text"]  # type: ignore
+
+    memory.save_messages_on_disk(chat_answer, session="test", new_chat=True)
 
 
-output = llm.create_chat_completion(
-    messages=prompt_from_scratch,
-    max_tokens=None,
-    stop=[
-        "<|end_of_turn|>"
-    ],  # Stop generating just before the model would generate a new question
-)
+def continue_chat():
 
+    memory = MessagesMemory()
+    prompt_with_history = memory.create_prompt_with_history(
+        user_message="Alright, I guess I'll ask ChatGPT then if you're too busy with your coffee.",
+        session="test",
+    )
 
-print(output["choices"])  # type: ignore
-print(output["usage"])  # type: ignore
+    llm = setup_llm(
+        ".llm_weights/openchat_3.5.04_K_M-gguf",
+        n_ctx=102,
+        chat_format="openchat",
+        verbose=False,
+    )
 
-chat_answer = output["choices"][0]["text"]  # type: ignore
+    output = llm.create_chat_completion(
+        messages=prompt_with_history,
+        max_tokens=None,
+        stop=[
+            "<|end_of_turn|>"
+        ],  # Stop generating just before the model would generate a new question
+    )
 
-memory.save_messages_on_disk(chat_answer, session="test", new_chat=False)
+    print(output["choices"])  # type: ignore
+    print(output["usage"])  # type: ignore
+
+    chat_answer = output["choices"][0]["text"]  # type: ignore
+
+    memory.save_messages_on_disk(chat_answer, session="test", new_chat=False)
