a94751177343bbbd2995d9795b9c1ddcba960b1e
Halil Zabun <halil.zabun@icloud.com>
2024-03-05 12:02:49 +0100

    feat: add continue session functionality

--- src/llm_dnd_dm/main.py
+++ src/llm_dnd_dm/main.py
@@ -19,13 +19,30 @@ def start_chat(session: str, system_message: str) -> None:
         # chatbot_answer = output["choices"][0]["message"]["content"]  # type: ignore
 
 
-# def continue_chat(session: str, system_message: str) -> None:
+def continue_chat(session: str, system_message: str) -> None:
 
-#     memory = SummaryBufferMemory(session=session, buffer_size=5)
+    dungeon_master = DungeonMaster(
+        session=session, system_message=system_message, new_chat=False
+    )
 
-#     run_loop_chat_instance(memory=memory, llm=llm)
+    while True:
+
+        user_message = input("What would you like to tell the dungeon master?\n")
+        print("-" * 200)
+
+        chatbot_answer = dungeon_master.respond_to_user(user_message=user_message)
+
+        print(chatbot_answer)
+
+        # chatbot_answer = output["choices"][0]["message"]["content"]  # type: ignore
 
 
-system_message = "The AI assistant is a worldclass chess player knowing all the tricks, but is also very busy at the moment drinking its coffee and doesn't want to be disturbed by anyone."
+chess_system_message = "The AI assistant is a worldclass chess player knowing all the tricks, but is also very busy at the moment drinking its coffee and doesn't want to be disturbed by anyone."
 
-start_chat(session="chess", system_message=system_message)
+pizza_system_message = (
+    "The AI assistant is an owner of a small italian pizza restaurant."
+)
+
+# start_chat(session="pizza", system_message=pizza_system_message)
+
+continue_chat(session="pizza", system_message=pizza_system_message)
